ed50dd555066795adfefce2a3ceb11f58c66622c
epistofan <andrejs.kolokuckis@inbox.lv>
2019-01-18 14:30:16 +0200

    visual

---
+++ src/main/resources/static/js/warning.js
@@ -0,0 +1,10 @@
+
+let div = document.createElement('div');
+div.className = "alert";
+if ('<#if response??>${response}</#if>'!= 0) {
+
+div.innerHTML = '<#if response??>${response}</#if>';
+
+document.body.appendChild(div);
+setTimeout(() => div.remove(), 2000);
+}
